d75a6712619ccf1de34db9db625bab0d55a8cfff
SangJoon Lee <qkqh34k@gmail.com>
2024-03-10 22:16:41 +0900

    .

--- src/test/java/com/iteratrlearning/chapter5/InspectorTest.java
+++ src/test/java/com/iteratrlearning/chapter5/InspectorTest.java
@@ -11,50 +11,6 @@ import static org.mockito.Mockito.verify;
 public class InspectorTest {
 
     @Test
-    public void shouldHaveNoRulesInitially() {
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
-        assertEquals(0, businessRuleEngine.count());
-    }
-    @Test
-    public void shouldAddTwoActions() {
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
-
-        businessRuleEngine.addAction(facts -> {
-            var dealStage = Stage.valueOf(facts.getFact("stage"));
-            var amount = Double.parseDouble(facts.getFact("amount"));
-            var forecatedAmount = amount * switch (dealStage){
-                case LEAD -> 0.2;
-                case EVALUATING -> 0.5;
-                case INTERESTED -> 0.8;
-                case CLOSED -> 1;
-            };
-            facts.setFact("forecastedAmount", String.valueOf(forecatedAmount));
-        });
-
-        assertEquals(2, businessRuleEngine.count());
-    }
-    @Test
-    public void shouldExecuteOneAction(){
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
-        final Action mockAction = mock(Action.class);
-
-        businessRuleEngine.addAction(mockAction);
-        businessRuleEngine.run();
-
-        verify(mockAction).execute();
-    }
-    @Test
-    public void shouldPerformAnActionWithFacts(){
-        final Action mockAction = mock(Action.class);
-        final Facts mockFacts = mock(Facts.class);
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(mockFacts);
-
-        businessRuleEngine.addAction(mockAction);
-        businessRuleEngine.run();
-
-        verify(mockAction).execute(mockFacts);
-    }
-    @Test
     public void inspectOneCOnditionEvaluatesTrue(){
         final Facts facts = new Facts();
         facts.setFact("jobTitle", "CEO");
@@ -78,4 +34,48 @@ public class InspectorTest {
             return "CEO".equals(facts.getFact("jobTitle"));
         }
     }
+//    @Test
+//    public void shouldHaveNoRulesInitially() {
+//        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+//        assertEquals(0, businessRuleEngine.count());
+//    }
+//    @Test
+//    public void shouldAddTwoActions() {
+//        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+//
+//        businessRuleEngine.addAction(facts -> {
+//            var dealStage = Stage.valueOf(facts.getFact("stage"));
+//            var amount = Double.parseDouble(facts.getFact("amount"));
+//            var forecatedAmount = amount * switch (dealStage){
+//                case LEAD -> 0.2;
+//                case EVALUATING -> 0.5;
+//                case INTERESTED -> 0.8;
+//                case CLOSED -> 1;
+//            };
+//            facts.setFact("forecastedAmount", String.valueOf(forecatedAmount));
+//        });
+//
+//        assertEquals(2, businessRuleEngine.count());
+//    }
+//    @Test
+//    public void shouldExecuteOneAction(){
+//        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+//        final Action mockAction = mock(Action.class);
+//
+//        businessRuleEngine.addAction(mockAction);
+//        businessRuleEngine.run();
+//
+//        verify(mockAction).execute();
+//    }
+//    @Test
+//    public void shouldPerformAnActionWithFacts(){
+//        final Action mockAction = mock(Action.class);
+//        final Facts mockFacts = mock(Facts.class);
+//        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(mockFacts);
+//
+//        businessRuleEngine.addAction(mockAction);
+//        businessRuleEngine.run();
+//
+//        verify(mockAction).execute(mockFacts);
+//    }
 }
